c1bc15d60bbc8510e1083fe831b13f9351ca2e02
Ilyan PASCOLINI <pascoliniilyan@gmail.com>
2024-10-08 18:23:19 +0200

    pb route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,6 @@
-import { createMemoryHistory, createRouter, createWebHistory } from 'vue-router'
-
+import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
-  history: createMemoryHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },]
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: []
 });
-
-export default router
+export default router;
